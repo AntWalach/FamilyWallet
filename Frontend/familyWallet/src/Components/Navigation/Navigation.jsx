7c3efcc9bcd8fb114101e7c9809016bff7768de8
bszarlowicz <b.szarlowicz@gmail.com>
2024-04-09 23:49:13 +0200

    [new] basic styles setup and navigation component

---
+++ Frontend/familyWallet/src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import layouts from "../../styles/layouts.module.css";
+import { menuItems } from '../../utils/menuItems';
+import { signout } from '../../utils/icons';
+import avatar from '../../images/avatar.png'
+
+
+function Navigation(){
+    return(
+        <div className={`${layouts.navigationStyled}`}>
+            <div className={`${layouts.userContainer}`}>
+                <img src={avatar} alt='avatar'/>
+                <div className={`${layouts.userText}`}>
+                    <h2>Adam</h2>
+                    <p>Your Money</p>
+                </div>
+            </div>
+            <ul className={`${layouts.menuItems}`}>
+                {menuItems.map((item) => {
+                    return <li
+                        key={item.id}                        
+                    >
+                        {item.icon}
+                        <span>{item.title}</span>
+                    </li>
+                })}
+            </ul>
+            <div className={`${layouts.bottomNav}`}>
+                <li>
+                    {signout} Sign Out
+                </li>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
